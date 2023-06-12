f6358c7292cc7afd329bba726312c3b180a5d97f
Fawaz <109764226+notfaw4z@users.noreply.github.com>
2023-06-12 12:11:43 -0400

    Update random_walk.py

--- Day 18/random_walk.py
+++ Day 18/random_walk.py
@@ -1,17 +1,25 @@
+import turtle
 from turtle import Turtle, Screen
 import random
 
-colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "brown", "cyan", "magenta"]
+turtle.colormode(255)
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r ,g , b)
+
 tim = Turtle()
 tim.shape("arrow")
 tim.pensize(15)
 tim.speed("fast")
 
-movement = [tim.fd(10), tim.bk(100)]
+
 directions = [0, 90, 180, 270]
 
 
 for _ in range(100):
     tim.fd(30)
     tim.setheading(random.choice(directions))
-    tim.color(random.choice(colors))
+    tim.color(random_color())
